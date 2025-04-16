a449e9131cdedea7321a5a093004793398bec1af
Edmilzon <edmilzon.luna@gmail.com>
2025-04-16 15:07:25 -0400

    sol descriptio car

--- src/components/recodeComponentes/RecodeDescripcionAuto.tsx
+++ src/components/recodeComponentes/RecodeDescripcionAuto.tsx
@@ -1,13 +1,14 @@
-export default function DescripcionAuto(){
+interface DescripcionAutoProps {
+    descripcion: string;
+}
+
+export default function DescripcionAuto({ descripcion }: DescripcionAutoProps) {
     return (
         <section className="w-full">
             <h2 className="text-2xl font-bold mb-4">Acerca de este auto</h2>
             <p className="text-gray-600 mb-6">
-                <br />
-                Descripción auto
-                <br />
-                <br />
+                {descripcion}
             </p>
         </section>
-    )
+    );
 }
